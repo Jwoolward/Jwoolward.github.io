dcba5e4432e0bfd01510662a2740dcfc8816b138
Jwoolward <Jwoolward.ptech@gmail.com>
2017-03-09 18:39:55 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+/* MAIN MENU */
+
+    $('#main-menu > li:has(ul.sub-menu)').addClass('parent');
+    $('ul.sub-menu > li:has(ul.sub-menu) > a').addClass('parent');
+
+    $('#menu-toggle').click(function() {
+        $('#main-menu').slideToggle(300);
+        return false;
+    });
+
+    $(window).resize(function() {
+        if ($(window).width() > 700) {
+            $('#main-menu').removeAttr('style');
+        }
+      });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* https://www.freshdesignweb.com/responsive-drop-down-menu-jquery-css3-using-icon-symbol/ /*
